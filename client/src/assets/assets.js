ccbdbe2a70544ff47341431aaf3e3ebfbad6864b
thatsDev <153229799+devenvicars@users.noreply.github.com>
2024-09-18 15:27:45 -0500

    Completed sidebar in navbar for scaling.

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -6,6 +6,8 @@ import parcel_icon from './parcel_icon.svg'
 import search_icon from './search_icon.png'
 import profile_icon from './profile_icon.png'
 import cart_icon from './cart_icon.png'
+import menu_icon from './menu_icon.png'
+import dropdown_icon from './dropdown_icon.png'
 
 export const assets = {
     logo,
@@ -16,5 +18,7 @@ export const assets = {
     search_icon,
     profile_icon,
     cart_icon,
+    menu_icon,
+    dropdown_icon
 }
 
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { NavLink, Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const NavBar = () => {
+
+    const [visible, setVisible] = useState(false);
+
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
 
@@ -48,7 +52,25 @@ const NavBar = () => {
                     <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>0</p>
                 </Link>
 
+                <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="mobile menu icon" />
             </div>
+
+            {/* Sidebar menu for small screen/mobile */}
+
+            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+                <div className='flex flex-col text-gray-700'>
+                    <div onClick={() => setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                        <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="dropdown icon" />
+                        <p>Back</p>
+                    </div>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+                </div>
+
+            </div>
+
         </div>
     )
 }
