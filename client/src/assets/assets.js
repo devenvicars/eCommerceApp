13687bf0daa4325839e0187800c88a473da7462c
thatsDev <153229799+devenvicars@users.noreply.github.com>
2024-09-18 14:03:43 -0500

    Completed navbar structure, before adding scalability.

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -3,12 +3,18 @@ import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
+import search_icon from './search_icon.png'
+import profile_icon from './profile_icon.png'
+import cart_icon from './cart_icon.png'
 
 export const assets = {
     logo,
     add_icon,
     order_icon,
     upload_area,
-    parcel_icon
+    parcel_icon,
+    search_icon,
+    profile_icon,
+    cart_icon,
 }
 
